76280accf10a24128bd6603ba14ddc98d555a242
Mohit <gangwanimohit42@gmail.com>
2024-02-11 01:42:46 +0530

    Made some corrections in ManageHotelForm folder

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,3 +1,75 @@
+// import { FormProvider, useForm } from "react-hook-form";
+// import DetailsSection from "./DetailsSection";
+// import TypeSection from "./TypeSection";
+// import FacilitiesSection from "./FacilitiesSection";
+// import GuestSection from "./GuestSection";
+// import ImageSection from "./ImageSection";
+
+// export type HotelFormData = {
+//   name: string;
+//   city: string;
+//   country: string;
+//   description: string;
+//   type: string;
+//   pricePerNight: number;
+//   starRating: number;
+//   facilities: string[];
+//   imageFiles: FileList;
+//   adultCount: number;
+//   childCount: number;
+// };
+// type props = {
+//   onSave: (HotelFormData: FormData) => void;
+//   isLoading: boolean;
+// };
+// const ManageHotelForm = ({ onSave, isLoading }: props) => {
+//   const formMethods = useForm<HotelFormData>();
+//   const { handleSubmit } = formMethods;
+
+//   const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+//     const formData = new FormData();
+//     formData.append("name", formDataJson.name);
+//     formData.append("city", formDataJson.city);
+//     formData.append("country", formDataJson.country);
+//     formData.append("description", formDataJson.description);
+//     formData.append("type", formDataJson.type);
+//     formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+//     formData.append("starRating", formDataJson.starRating.toString());
+//     formData.append("adultCount", formDataJson.adultCount.toString());
+//     formData.append("childCount", formDataJson.childCount.toString());
+
+//     formDataJson.facilities.forEach((facility, index) => {
+//       formData.append(`facilities[${index}]`, facility);
+//     });
+//     Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+//       formData.append(`imageFiles`, imageFile);
+//     });
+//     onSave(formData);
+//   });
+//   return (
+//     <FormProvider {...formMethods}>
+//       <form onSubmit={onSubmit} className="flex flex-col gap-10">
+//         <DetailsSection />
+//         <TypeSection />
+//         <FacilitiesSection />
+//         <GuestSection />
+//         <ImageSection />
+//         <span className="flex justify-end">
+//           <button
+//             disabled={isLoading}
+//             type="submit"
+//             className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 disabled:bg-gray-500"
+//           >
+//             {isLoading ? "Saving..." : "Save"}
+//           </button>
+//         </span>
+//       </form>
+//     </FormProvider>
+//   );
+// };
+
+// export default ManageHotelForm;
+
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
@@ -18,11 +90,16 @@ export type HotelFormData = {
   adultCount: number;
   childCount: number;
 };
-type props = {
-  onSave: (HotelFormData: FormData) => void;
+
+interface ManageHotelFormProps {
+  onSave: (formData: FormData) => void;
   isLoading: boolean;
-};
-const ManageHotelForm = ({ onSave, isLoading }) => {
+}
+
+const ManageHotelForm: React.FC<ManageHotelFormProps> = ({
+  onSave,
+  isLoading,
+}) => {
   const formMethods = useForm<HotelFormData>();
   const { handleSubmit } = formMethods;
 
@@ -46,6 +123,7 @@ const ManageHotelForm = ({ onSave, isLoading }) => {
     });
     onSave(formData);
   });
+
   return (
     <FormProvider {...formMethods}>
       <form onSubmit={onSubmit} className="flex flex-col gap-10">
